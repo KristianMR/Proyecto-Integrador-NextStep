9175f4571e2a1e4a49b8c590f44c8593785c1003
KristianMR <kmogrovejo@ucundinamarca.edu.co>
2025-05-17 19:35:08 -0500

    Update app.js

--- Proyecto_NextStep/backend/src/app.js
+++ Proyecto_NextStep/backend/src/app.js
@@ -16,5 +16,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-    console.log('servidor corriendo en el puerto ${PORT}')
+    console.log('servidor corriendo en el puerto ' + PORT)
 });
